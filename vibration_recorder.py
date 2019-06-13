1f3c2ecfd9e4679c8e48def0449c0780a93c1b0d
yangxiao <tristonyang@gmail.com>
2019-06-13 11:43:51 -0400

    loop

--- vibration_recorder.py
+++ vibration_recorder.py
@@ -1,12 +1,29 @@
 from sense_hat import SenseHat
 from datetime import datetime
+from csv import writer
+import csv
+
+filename = "vibration.csv"
+header = ['acc_x', 'acc_y', 'acc_z', 'time']
 
 sense = SenseHat()
 
 def get_acc_data():
     data = []
-    data.append(sense.get_accelerometer_raw())
+    acc = sense.get_accelerometer_raw()
+    data.append(acc["x"])
+    data.append(acc["y"])
+    data.append(acc["z"])
     data.append(datetime.now())
     return(data)
 
 print(get_acc_data())
+
+with open(filename, 'w', newline = '') as f:
+    data_writer = writer(f)
+    data_writer.writerow(header)
+
+    while i in range(0, 100):
+        data = get_acc_data()
+        print(data)
+        data_writer.writerow(data)
